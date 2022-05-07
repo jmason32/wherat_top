ce2566813f0faa6ad147e3163fdfe11edf701544
Jordan Mason <jordanmason@Jordans-MBP.lan>
2022-05-07 16:54:49 -0400

    client start

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hello 
+
+      yooo
+
+      WE DA BEST
+    </div>
+  );
+}
+
+export default App;
